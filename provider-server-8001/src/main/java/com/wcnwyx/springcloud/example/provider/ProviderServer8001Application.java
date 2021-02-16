49b62eaf44699b0200520380e0c195bdb8d112d0
cheng.wu <1122wucheng@163.com>
2021-02-16 21:41:31 +0800

    'provider-server'

--- provider-server-8001/src/main/java/com/wcnwyx/springcloud/example/provider/ProviderServer8001Application.java
+++ provider-server-8001/src/main/java/com/wcnwyx/springcloud/example/provider/ProviderServer8001Application.java
@@ -2,12 +2,12 @@ package com.wcnwyx.springcloud.example.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"com.wcnwyx.springcloud.example.provider"})
-public class ProviderApplication {
+public class ProviderServer8001Application {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderApplication.class);
+        SpringApplication.run(ProviderServer8001Application.class);
     }
 }
---
+++ provider-server-8002/src/main/java/com/wcnwyx/springcloud/example/provider/ProviderServer8002Application.java
@@ -0,0 +1,14 @@
+package com.wcnwyx.springcloud.example.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = {"com.wcnwyx.springcloud.example.provider"})
+public class ProviderServer8002Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderServer8002Application.class);
+    }
+}
