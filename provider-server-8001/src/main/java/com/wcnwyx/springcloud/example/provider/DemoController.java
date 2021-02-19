b5cd68be3760da274e39461c145ee297f910c822
cheng.wu <1122wucheng@163.com>
2021-02-19 11:23:31 +0800

    'dashboard'

--- provider-server-8001/src/main/java/com/wcnwyx/springcloud/example/provider/DemoController.java
+++ provider-server-8001/src/main/java/com/wcnwyx/springcloud/example/provider/DemoController.java
@@ -1,11 +1,8 @@
 package com.wcnwyx.springcloud.example.provider;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 @RestController
 public class DemoController {
@@ -14,17 +11,7 @@ public class DemoController {
     private int port;
 
     @RequestMapping("/demo")
-    @HystrixCommand(fallbackMethod = "demoFallback")
     public String demo(){
-        int i = new Random().nextInt(2);
-        if(i==0){
-            return "This is Demo. port="+port;
-        }else{
-            throw new RuntimeException();
-        }
-    }
-
-    public String demoFallback(){
-        return "this is HystrixCommand fallback.";
+        return "This is Demo. port="+port;
     }
 }
--- provider-server-hystrix-8004/src/main/java/com/wcnwyx/springcloud/example/provider/ProviderServerHystrix8004Application.java
+++ provider-server-hystrix-8004/src/main/java/com/wcnwyx/springcloud/example/provider/ProviderServerHystrix8004Application.java
@@ -3,11 +3,13 @@ package com.wcnwyx.springcloud.example.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
+@EnableHystrix
 @SpringBootApplication
-public class ProviderServer8001Application {
+public class ProviderServerHystrix8004Application {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderServer8001Application.class, args);
+        SpringApplication.run(ProviderServerHystrix8004Application.class, args);
     }
 }
