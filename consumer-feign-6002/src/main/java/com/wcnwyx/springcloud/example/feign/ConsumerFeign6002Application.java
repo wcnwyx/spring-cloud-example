97a0cf9cd1292ddd7be7d6d40a97e0eaab79f2d6
wucheng <wc@candypay.com>
2021-03-19 17:20:38 +0800

    feign

--- consumer-feign-6002/src/main/java/com/wcnwyx/springcloud/example/feign/ConsumerFeign6002Application.java
+++ consumer-feign-6002/src/main/java/com/wcnwyx/springcloud/example/feign/ConsumerFeign6002Application.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //当feignClient不在该包目录及其子目录下是，通过EnableFeignClients的basePackages来指定目录
 //feignClient里定义的fallback或者fallbackFactory也需要通过scanBasePackages来指定目录扫描
 @EnableFeignClients(basePackages = {"com.wcnwyx.springcloud.example.api"})
-@EnableHystrix
+//@EnableHystrix
 @SpringBootApplication(scanBasePackages = {"com.wcnwyx.springcloud.example.api","com.wcnwyx.springcloud.example.feign"})
 public class ConsumerFeign6002Application {
     public static void main(String[] args) {
