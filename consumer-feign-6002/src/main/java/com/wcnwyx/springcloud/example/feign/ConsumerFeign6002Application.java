2aaad86dfd9f9bbcc585ce627850df091e575fd0
cheng.wu <1122wucheng@163.com>
2021-04-25 22:40:00 +0800

    ip.md

--- consumer-feign-6002/src/main/java/com/wcnwyx/springcloud/example/feign/ConsumerFeign6002Application.java
+++ consumer-feign-6002/src/main/java/com/wcnwyx/springcloud/example/feign/ConsumerFeign6002Application.java
@@ -2,11 +2,11 @@ package com.wcnwyx.springcloud.example.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 //当feignClient不在该包目录及其子目录下是，通过EnableFeignClients的basePackages来指定目录
 //feignClient里定义的fallback或者fallbackFactory也需要通过scanBasePackages来指定目录扫描
 @EnableFeignClients(basePackages = {"com.wcnwyx.springcloud.example.api"})
